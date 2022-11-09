f51b86fe48a1e905d51a7fc421d4e1b72039efdd
Ligia Keiko <70450947+keikoligia@users.noreply.github.com>
2022-11-09 10:51:53 -0300

    remove node_modules

--- APP/app_ajuda_ubs/android/app/src/main/kotlin/com/example/app_ajuda_aubs/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_ajuda_aubs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- APP/app_ajuda_ubs/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app/utils/app_rotas.dart';
-
-main() {
-  runApp(const AppRotas());
-}
